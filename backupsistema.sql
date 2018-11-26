5033942544654614687a6e9bb4a2e14f57b0092e
BarretoMillena <39960143+BarretoMillena@users.noreply.github.com>
2018-11-26 18:56:50 -0200

    Add files via upload

--- backupsistema.sql
+++ backupsistema.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.5.7
 -- Dumped by pg_dump version 9.5.7
 
--- Started on 2018-11-26 18:49:24 BRST
+-- Started on 2018-11-26 18:14:35 BRST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,16 +16,33 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 8 (class 2615 OID 33006)
--- Name: sistemasaude; Type: SCHEMA; Schema: -; Owner: postgres
+-- TOC entry 7 (class 2615 OID 33006)
+-- Name: paciente; Type: SCHEMA; Schema: -; Owner: postgres
 --
 
-CREATE SCHEMA sistemasaude;
+CREATE SCHEMA paciente;
 
 
-ALTER SCHEMA sistemasaude OWNER TO postgres;
+ALTER SCHEMA paciente OWNER TO postgres;
 
-SET search_path = sistemasaude, pg_catalog;
+--
+-- TOC entry 1 (class 3079 OID 12395)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2165 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = paciente, pg_catalog;
 
 SET default_tablespace = '';
 
@@ -33,12 +50,12 @@ SET default_with_oids = false;
 
 --
 -- TOC entry 182 (class 1259 OID 33008)
--- Name: calendario; Type: TABLE; Schema: sistemasaude; Owner: postgres
+-- Name: calendario; Type: TABLE; Schema: paciente; Owner: postgres
 --
 
 CREATE TABLE calendario (
-    nm_descricao character varying,
-    dt_data character varying(8)
+    nr_data date,
+    nm_descricao text
 );
 
 
@@ -46,13 +63,13 @@ ALTER TABLE calendario OWNER TO postgres;
 
 --
 -- TOC entry 185 (class 1259 OID 33026)
--- Name: login; Type: TABLE; Schema: sistemasaude; Owner: postgres
+-- Name: login; Type: TABLE; Schema: paciente; Owner: postgres
 --
 
 CREATE TABLE login (
-    ds_senha character(10),
+    ds_senha character(1),
     nm_nomecompleto text,
-    ds_email character varying(30)
+    nm_email text
 );
 
 
@@ -60,13 +77,13 @@ ALTER TABLE login OWNER TO postgres;
 
 --
 -- TOC entry 183 (class 1259 OID 33014)
--- Name: medico; Type: TABLE; Schema: sistemasaude; Owner: postgres
+-- Name: medico; Type: TABLE; Schema: paciente; Owner: postgres
 --
 
 CREATE TABLE medico (
-    nm_nomecompleto text,
-    ds_email character varying,
-    ds_senha numeric(10,0),
+    nm_nomecompleto text
+    ds_email text,
+    ds_senha numeric,
     ds_fichadopaciente character varying(1),
     nm_especialidade text
 );
@@ -76,13 +93,13 @@ ALTER TABLE medico OWNER TO postgres;
 
 --
 -- TOC entry 184 (class 1259 OID 33020)
--- Name: paciente; Type: TABLE; Schema: sistemasaude; Owner: postgres
+-- Name: paciente; Type: TABLE; Schema: paciente; Owner: postgres
 --
 
 CREATE TABLE paciente (
     nm_nomecompleto text,
     ds_senha numeric,
-    ds_email character varying,
+    ds_email text,
     ds_datanascimento date,
     ds_convenio numeric
 );
@@ -92,7 +109,7 @@ ALTER TABLE paciente OWNER TO postgres;
 
 --
 -- TOC entry 186 (class 1259 OID 33032)
--- Name: pasta; Type: TABLE; Schema: sistemasaude; Owner: postgres
+-- Name: pasta; Type: TABLE; Schema: paciente; Owner: postgres
 --
 
 CREATE TABLE pasta (
@@ -108,54 +125,53 @@ ALTER TABLE pasta OWNER TO postgres;
 --
 -- TOC entry 2153 (class 0 OID 33008)
 -- Dependencies: 182
--- Data for Name: calendario; Type: TABLE DATA; Schema: sistemasaude; Owner: postgres
+-- Data for Name: calendario; Type: TABLE DATA; Schema: paciente; Owner: postgres
 --
 
-COPY calendario (nm_descricao, dt_data) FROM stdin;
-\.
 
 
 --
 -- TOC entry 2156 (class 0 OID 33026)
 -- Dependencies: 185
--- Data for Name: login; Type: TABLE DATA; Schema: sistemasaude; Owner: postgres
+-- Data for Name: login; Type: TABLE DATA; Schema: paciente; Owner: postgres
 --
 
-COPY login (ds_senha, nm_nomecompleto, ds_email) FROM stdin;
-\.
 
 
 --
 -- TOC entry 2154 (class 0 OID 33014)
 -- Dependencies: 183
--- Data for Name: medico; Type: TABLE DATA; Schema: sistemasaude; Owner: postgres
+-- Data for Name: medico; Type: TABLE DATA; Schema: paciente; Owner: postgres
 --
 
-COPY medico (nm_nomecompleto, ds_email, ds_senha, ds_fichadopaciente, nm_especialidade) FROM stdin;
-\.
 
 
 --
 -- TOC entry 2155 (class 0 OID 33020)
 -- Dependencies: 184
--- Data for Name: paciente; Type: TABLE DATA; Schema: sistemasaude; Owner: postgres
+-- Data for Name: paciente; Type: TABLE DATA; Schema: paciente; Owner: postgres
 --
-
-COPY paciente (nm_nomecompleto, ds_senha, ds_email, ds_datanascimento, ds_convenio) FROM stdin;
-\.
 
 
 --
 -- TOC entry 2157 (class 0 OID 33032)
 -- Dependencies: 186
--- Data for Name: pasta; Type: TABLE DATA; Schema: sistemasaude; Owner: postgres
+-- Data for Name: pasta; Type: TABLE DATA; Schema: paciente; Owner: postgres
 --
 
-COPY pasta (nm_receita, nm_prontuario, nm_exames, nr_diasconsulta) FROM stdin;
-\.
+--
+-- TOC entry 2164 (class 0 OID 0)
+-- Dependencies: 8
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2018-11-26 18:49:24 BRST
+-- Completed on 2018-11-26 18:14:36 BRST
 
 --
 -- PostgreSQL database dump complete
